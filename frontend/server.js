d3ef4f91b926f131a56576ad2af18f997e32b474
GAUTAM-2808 <141563380+GAUTAM-2808@users.noreply.github.com>
2026-04-02 14:07:23 +0530

    Add demo frontend for API testing

---
+++ frontend/server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/' || req.url === '/index.html') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading index.html');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Not found');
+  }
+});
+
+server.listen(8080, () => {
+  console.log('Frontend server running on http://localhost:8080');
+});
